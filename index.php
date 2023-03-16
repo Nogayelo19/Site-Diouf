28e758d80d252bd10408b5f8fac45cc4ae4ae370
AN200014 <astoumoubayam@gmail.com>
2023-03-16 00:40:26 +0000

    Ajout du projét

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Green Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="public/templates/template_vitrine/assets/img/favicon.png" rel="icon">
+  <link href="public/templates/template_vitrine/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="public/templates/template_vitrine/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="public/templates/template_vitrine/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="public/templates/template_vitrine/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="public/templates/template_vitrine/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="public/templates/template_vitrine/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="public/templates/template_vitrine/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="public/templates/template_vitrine/assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Green - v4.10.0
+  * Template URL: https://bootstrapmade.com/green-free-one-page-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+
+
+  <!-- ========= Head ========= -->
+  <?php require_once('view/partials/extract_vitrine/menu.php') ?>
+  <!-- ======= End Head ======= -->
+
+  
+
+
+  <!-- ======= Hero Section ======= -->
+  <?php require_once('view/partials/extract_vitrine/heroSection.php') ?>
+
+  <main id="main">
+ <!-- ======= Featured Services Section ======= -->
+
+
+<!-- ======= About Us Section ======= -->
+  <?php require_once('view/partials/extract_vitrine/Apropos.php') ?>
+<!-- End About Us Section -->
+
+<!-- ======= Partinaire Section ======= -->
+<?php require_once('view/partials/extract_vitrine/partiniare.php') ?>
+ 
+<!-- Partinaire Section -->
+
+<!-- ======= Services Section ======= -->
+<?php require_once('view/partials/extract_vitrine/service.php') ?>
+   
+<!-- End Services Section -->
+
+<!-- ======= Portfolio Section ======= -->
+<?php require_once('view/partials/extract_vitrine/Portfolio.php') ?>
+<!-- End Portfolio Section -->
+
+ <!-- ======= Team Section ======= -->
+<?php require_once('view/partials/extract_vitrine/equipe.php') ?>
+<!-- End Team Section -->
+
+<!-- ======= Contact Section ======= -->
+<?php require_once('view/partials/extract_vitrine/contact.php') ?>
+<!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+<!-- ======= Footer ======= -->
+  <?php require_once('view/partials/extract_vitrine/footer.php') ?>
+<!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="public/templates/template_vitrine/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="public/templates/template_vitrine/assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="public/templates/template_vitrine/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="public/templates/template_vitrine/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="public/templates/template_vitrine/assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="public/templates/template_vitrine/assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ view/partials/extract_vitrine/Apropos.php
@@ -0,0 +1,78 @@
+<section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>About Us</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2">
+            <img src="public/templates/template_vitrine/assets/img/about.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+            <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
+            <p class="fst-italic">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua.
+            </p>
+            <ul>
+              <li><i class="bi bi-check-circled"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+              <li><i class="bi bi-check-circled"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+              <li><i class="bi bi-check-circled"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+            </ul>
+            <p>
+              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <section id="why-us" class="why-us">
+      <div class="container">
+
+        <div class="row no-gutters">
+
+          <div class="col-lg-4 col-md-6 content-item">
+            <span>01</span>
+            <h4>Lorem Ipsum</h4>
+            <p>Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et consectetur ducimus vero placeat</p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 content-item">
+            <span>02</span>
+            <h4>Repellat Nihil</h4>
+            <p>Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 content-item">
+            <span>03</span>
+            <h4> Ad ad velit qui</h4>
+            <p>Molestiae officiis omnis illo asperiores. Aut doloribus vitae sunt debitis quo vel nam quis</p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 content-item">
+            <span>04</span>
+            <h4>Repellendus molestiae</h4>
+            <p>Inventore quo sint a sint rerum. Distinctio blanditiis deserunt quod soluta quod nam mider lando casa</p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 content-item">
+            <span>05</span>
+            <h4>Sapiente Magnam</h4>
+            <p>Vitae dolorem in deleniti ipsum omnis tempore voluptatem. Qui possimus est repellendus est quibusdam</p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 content-item">
+            <span>06</span>
+            <h4>Facilis Impedit</h4>
+            <p>Quis eum numquam veniam ea voluptatibus voluptas. Excepturi aut nostrum repudiandae voluptatibus corporis sequi</p>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
